fd54f15b864997272c49a0af7d65e3b444f95aa1
Kartik Bevoor <kartikbevoor2130@gmail.com>
2026-02-15 11:33:31 +0530

    struct completed

--- struct.go
+++ struct.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // A struct is a user-defined type that groups together variables (fields) under one name.
 func structs() {
@@ -66,6 +68,37 @@ func structs() {
 		cardNo:             1234,
 	}
 	card.bankAccountDetails.accType = "current"
+
+	// nested struct
+	order1 := order{
+		id: 34,
+		p1: person{name: "sdfs", age: 23},
+	}
+	fmt.Println(order1)
+
+	// shalow copy
+	karan := relative1{
+		character: "chutiya",
+	}
+
+	samar := karan
+	samar.character = "not chutiya" // karan and samar are completely independent, changing samar char does affect karan
+
+	// problem with shallow copy when reference types are used
+	cric1 := cricketer2{
+		name:   "Don",
+		scores: []int{45, 34},
+	}
+
+	cric2 := cric1
+	cric2.scores[0] = 100
+	fmt.Println(cric1.scores[0]) // 100 : here the go copied the slice header
+
+	// deep copy manually
+	cric3 := cric1
+	cric3.scores = make([]int, len(cric1.scores))
+	copy(cric3.scores, cric1.scores)
+
 }
 
 type employee struct {
@@ -138,4 +171,90 @@ type cardDetails struct {
 	personEmbedded     personEmbedded
 	bankAccountDetails bankAccountDetails
 	cardNo             int
+}
+
+// struct tags: are string annotations attached to struct fields that provide metadata.
+type developer struct {
+	Name string `json:"name"` // The part inside backticks is the struct tag.
+	Age  int    `json:"-"`
+}
+
+// struts are comparible
+// p1 == p2
+// Not allowed if:
+// Contains slice
+// Contains map
+// Contains function
+
+// nested struct
+type order struct {
+	id int
+	p1 person
+}
+
+// Note: embedded structs does not include field name nested struct does include field name
+
+// exported v/s unexported struct fieds
+type addhar struct {
+	Id   int    // exported field written using capital letters(public)
+	name string // unexported field written using small lettes(private)
+}
+
+// empty struct
+type empty struct{}
+
+// Padding = extra unused bytes inserted to make the next field start at a properly aligned memory address.
+// bad order
+type Example struct {
+	A bool   // 1byte
+	B int    // 8 byte
+	C string // 16 byte
+}
+
+// total mem used: 1(A) + 7(padding) + 8(b) + 16(c) = 32
+
+// good order
+type example2 struct {
+	c string
+	b int
+	a bool
+}
+
+// total mem used: 16(c) + 8(b) + 1(a) + 7(padding) = 32
+
+type Bad struct {
+	A bool
+	B int64
+}
+
+type Good struct {
+	B int64
+	A bool
+}
+
+// constructors: go has no constructors but use functions
+func exConstructor(name string, age int) *person {
+	return &person{
+		name: name,
+		age:  age,
+	}
+}
+
+// Struct Copy vs Deep Copy
+// In Go, when you assign one struct to another:
+// b := a
+// Go performs a shallow copy (value copy).
+// That means:
+// All fields are copied.
+// But if a field contains a reference type, the reference is copied — not the underlying data.
+
+// shallow copy(when copying value type)
+type relative1 struct {
+	character string
+}
+
+// deep copy (when copying reference type)
+type cricketer2 struct {
+	name   string
+	scores []int
 }
